32395162fde3754246b52071f2ae49a87f309102
sona1318 <krithisonakrithika@gmail.com>
2026-07-08 08:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.23%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,10 +1,10 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        steps = 0
+        steps = 0  #stop when it become 0
         while num > 0:
-            if num % 2 == 0:
-                num //= 2
+            if num % 2 == 0: #if number is even % 2
+                num //= 2   #to remove decimal number
             else:
-                num -= 1
-            steps += 1
+                num -= 1    #if num is even -1
+            steps += 1 #to know how many steps used
         return steps
